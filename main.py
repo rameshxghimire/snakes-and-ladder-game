32cc81d6bb3b25c94f30700744dc8ba36a625db1
Ramesh Ghimire <rameshxghimire@gmail.com>
2019-01-14 15:58:31 +0200

    Grid modifications.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+ This is the main file.
+
+This will be the main file for the CLI and all game logic.
+
+"""
+import random
+import datetime
+from snl-main import *
+
+
+def welcome_screen():
+    """
+    welcome_screen initialize the game interface.
+    First time intro the game to the users.
+    """
--- snl-main.py
+++ snl-main.py
@@ -1,5 +1,5 @@
 """
-snl-main.py: Main file for the game logic.
+This file is for making the grid.
 """
 
 from colors import print_custom
@@ -31,11 +31,31 @@ from colors import print_custom
 # for rows in make_rows(size):
 #     print(rows)
 
-# * Create Grid: Method 3
-rows = []
-for n in range(81, 0, -20):
-    rows.append(list(range(n + 19, n + 9, -1)))  # Going from 100 to 90 in the first list
-    rows.append(list(range(n, n + 10)))
 
-for row in rows:
-    print_custom(' | '.join([f"{col:03}" for col in row]))  # use print_custom function from colors module to get the output in color.
+# * Create Grid: Method 3
+# Show the grid
+def show_grid():
+    """
+    show_grid Shows the main grid.
+
+    Initialize this grid when the game begins.
+
+    """
+
+    # Define snakes' and ladders' positions
+    snakes = {99: 3, 75: 48, 68: 23, 44: 17, 30: 5}
+    ladders = {3: 'L35', 8: 'L28', 19: 'L39', 27: 'L65', 43: 'L78', 69: 'L96'}
+    # Create a list for the grid
+    global rows
+    rows = []
+    for n in range(81, 0, -20):
+        rows.append(list(range(n + 19, n + 9, -1)))  # Going from 100 to 90 in the first list
+        rows.append(list(range(n, n + 10)))
+    # ? Adding snakes and ladders in the grid
+    pass
+
+    for row in rows:
+        print_custom(' | '.join([f'{col:03}' for col in row]))  # use print_custom function from colors module to get the output in color.
+
+# Test if it works
+show_grid()
